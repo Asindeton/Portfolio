550982f6d1c82dcc5d0dbcfa393bb4287bba4e48
Дмитрий Леганьков <you@example.com legankov95@gmail.comgit config --global user.name Asindetongit commit -m 'test cv'$ git config --global user.email you@example.com>
2019-10-19 00:07:37 +0300

    feat:add fFunctional

---
+++ assets/examples/repair-design-project/index.js
@@ -0,0 +1,22 @@
+function change( el )
+{
+    if ( el.value === "desktop" )
+        el.value = "mobile";
+    else
+        el.value = "desktop";
+}
+
+let toMobile = document.querySelectorAll('.wrapper');
+
+const button = document.getElementById('button-change-version');
+let counter = 0;
+
+button.addEventListener('click', function(){
+    const content = document.getElementById('content')
+    if(counter == 0 || counter%2==0) {
+    content.className = 'to-mobile-version'
+} else{
+    content.className = 'to-desktop-version'
+    }
+    counter++;
+});
